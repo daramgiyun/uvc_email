78d52d6dc2d210dd8bc584ce55628f3061fff1e7
daramjiyun <315ljy@gmail.com>
2023-01-04 10:37:57 +0900

    grid 완성

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,22 @@
 <template>
   <div class="container">
-    <div class="sendInfo">
-      <div class="one">보내는사람</div>
-      <div class="two">받는사람</div>
+    <div class="a">
+      <div class="one">보내는 사람</div>
+      <div class="two">받는 사람</div>
       <div class="three">제목</div>
-      <div class="four">첨부파일</div>
-      <div class="five">끌어오기</div>
-      <div class="one2">
-        <input type="text" class="input" /><button>계정</button>
-      </div>
-      <div class="two2">
-        <input type="text" class="input" /><button>주소록</button>
-      </div>
-      <div class="three2"><input type="text" class="input" /></div>
+      <div class="four">첨부 파일</div>
+    </div>
+    <div class="b">
+      <div class="one2"><input type="text" /></div>
+      <div class="two2"><input type="text" /></div>
+      <div class="three2"><input type="text" /></div>
       <div class="four2"><button>내 PC</button></div>
-      <div class="five2"><div>마우스로 끌어오기</div></div>
+      <div class="four3">끌어오기</div>
     </div>
-    <div class="six">
-      <textarea
-        class="mail"
-        name="mail"
-        id="mail"
-        cols="155px"
-        rows="20px"
-      ></textarea>
-    </div>
+    <div class="six">내용</div>
     <div class="seven">
       <button>취소</button>
-      <button>전송</button>
+      <button>보내기</button>
     </div>
   </div>
 </template>
@@ -37,102 +26,90 @@ export default {}
 </script>
 
 <style>
-.container {
-  display: grid; 
-  grid-template-columns: 100%; 
-  grid-template-rows: 270px 530px 100px; 
-  gap: 0px 0px; 
-  grid-template-areas: 
-    "sendInfo"
-    "six"
-    "seven"; 
-  margin-top: 1%;
-  /* text-align: center; */
-  /* align-items: center; */
+.container {  display: grid;
+  grid-template-columns: 20% 80%;
+  grid-template-rows: 270px 560px 70px;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "a b"
+    "six six"
+    "seven seven";
   background-color: lightcyan;
+  margin-top: 2%;
 }
-.sendInfo {
-  display: grid; 
-  grid-template-columns: 30% 70%; 
-  grid-template-rows: 50px 50px 50px 50px 70px; 
-  gap: 0px 0px; 
-  grid-template-areas: 
-    "one one2"
-    "two two2"
-    "three three2"
-    "four four2"
-    "five five2"; 
-  grid-area: sendInfo; 
+
+.a {  display: grid;
+  grid-template-rows: 55px 55px 55px 105px;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-area: a;
+  background-color: lightseagreen;
   text-align: center;
   align-items: center;
-  background-color: lightseagreen ;
 }
-.one { 
-  grid-area: one;
-  /* background-color: lightcoral; */
-  /* border-style: solid; */
+
+.one {
+  grid-area: 1 / 1 / 2 / 2;
+  background-color: lightcoral;
  }
+
 .two {
-   grid-area: two;
-   /* background-color: lightgoldenrodyellow; */
-   /* border-style: solid; */
+   grid-area: 2 / 1 / 3 / 2; 
+   background-color: lightgoldenrodyellow;
    }
-.three { 
-  grid-area: three;
-  /* background-color: lightcoral; */
-  /* border-style: solid; */
- }
+
+.three {
+   grid-area: 3 / 1 / 4 / 2; 
+    background-color: lightcoral;
+
+   }
+
 .four {
-   grid-area: four; 
-   /* background-color: lightgoldenrodyellow; */
-   /* border-style: solid; */
-  }
-.five {
-   grid-area: five; 
-   /* background-color: lightcoral; */
-   /* border-style: solid; */
-  }
-.six { 
-  grid-area: six; 
-  /* background-color: lightgoldenrodyellow; */
-  /* border-style: solid; */
-}
-.seven {
-   grid-area: seven;
-   /* background-color: lightcoral; */
-   /* border-style: solid; */
+   grid-area: 4 / 1 / 5 / 2; 
+   background-color: lightgoldenrodyellow;
    }
-.one2{
-  grid-area: one2;
-  background-color: lightblue;
-  /* border-style: solid; */
+
+.b {
+  display: grid;
+  grid-template-rows: 55px 55px 55px 30px 75px;
+  grid-auto-columns: 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-area: b;
+  background-color: lightgreen;
+  /* text-align: center; */
+  /* align-items: center; */
 }
-.two2{
-  grid-area: two2;
-  /* background-color: lightblue; */
-  /* border-style: solid; */
+
+.one2 { 
+  grid-area: 1 / 1 / 2 / 2;
+  background-color: lightgoldenrodyellow;
+  }
+
+.two2 { grid-area: 2 / 1 / 3 / 2; 
+    background-color: lightcoral;
 }
-.three2{
-  grid-area: three2;
-  /* background-color: lightblue; */
-  /* border-style: solid; */
-}
-.four2{
-  grid-area: four2;
-  /* background-color: lightblue; */
-  /* border-style: solid; */
-}
-.five2{
-  grid-area: five2;
-  /* background-color: lightblue; */
-  /* border-style: solid; */
-}
-.mail{
-  margin: 1% 1% 1% 1%;
-  resize:none;/* 크기고정 */ 
-  overflow: auto;
-}
-.input{
-  width: 800px;
-}
+
+.three2 {
+  grid-area: 3 / 1 / 4 / 2; 
+  background-color: lightgoldenrodyellow;
+  }
+
+.four2 { grid-area: 4 / 1 / 5 / 2;
+    background-color: lightcoral;
+ }
+
+.four3 {
+   grid-area: 5 / 1 / 6 / 2;
+   background-color: lightcoral;
+   }
+
+.six { grid-area: six; 
+background-color: lightslategray;}
+
+.seven {
+   grid-area: seven; 
+   background-color: lightgoldenrodyellow;}
+
 </style>
